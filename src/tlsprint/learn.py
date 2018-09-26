821e0d00ee5d26fdec3c6dbfcd413f8c22b1a416
Erwin Janssen <erwinjanssen@outlook.com>
2018-09-26 20:11:37 +0200

    Move package to 'src' directory
    This is in line with the current best practice.

---
+++ src/tlsprint/learn.py
@@ -0,0 +1,12 @@
+def learn(directory):
+    """Learn the complete tree from all the different models.
+
+    Args:
+        directory: A directory that contains the output of StateLearner. It
+            expects the directory to contain directories with the names of the
+            different TLS implementations, where each TLS directory contains a
+            file 'learnedModel.dot'. Will skip implementations where this file
+            is absent.
+    """
+    subdirs = [f.name for f in os.scandir(directory) if f.is_dir()]
+    return subdirs
